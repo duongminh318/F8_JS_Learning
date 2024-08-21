5e22237d27eb5bbbba8b2a715df19308a577fb8d
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-08-21 15:37:57 +0700

    completed ex loop2 : loop in array

--- f8_js_pro/loop.js
+++ f8_js_pro/loop.js
@@ -71,3 +71,67 @@ sử dụng vòng lặp for.
 
     // Sample usage
     // console.log(findMin([5, 2, 8, 1, 4])); // 1
+
+    /**
+     * Tổng hợp bài tập vòng lặp For #2 
+1. Viết hàm countPositiveNumbers nhận vào một mảng số và 
+trả về số lượng số dương trong mảng đó.
+2. Viết hàm sumArray nhận vào một mảng số và trả về tổng của 
+tất cả các số trong mảng đó.
+3. Viết hàm calculateAverage nhận vào một mảng số và trả về 
+trung bình cộng của các số trong mảng đó.
+     */
+
+/**
+ * 1. Counts the number of positive numbers in an array.
+ *
+ * @param {Array<number>} numbers - The array of numbers
+ * @returns {number} The count of positive numbers
+ */
+function countPositiveNumbers (arr){
+
+    let countPositive=0;
+    for(let i=0; i<arr.length;i++){
+        if(arr[i]>0){
+            countPositive++;
+        }
+    }
+    return countPositive;
+}
+
+// Sample usage
+console.log(countPositiveNumbers([-1, 2, 3, -4, 5])); // 3
+
+/**
+ * 2. Calculates the sum of all numbers in an array.
+ *
+ * @param {Array<number>} arr - The array of numbers
+ * @returns {number} The sum of the numbers
+ */
+function sumArray  (arr){
+
+    let sumOfArray=0;
+    for(let i=0; i<arr.length;i++){   
+            sumOfArray+=arr[i];        
+    }
+    return sumOfArray;
+}
+
+// Sample usage
+// console.log(sumArray([1, 2, 3, 4, 5])); // 15
+
+/**
+ * 3. Calculates the average of numbers in an array.
+ *
+ * @param {Array<number>} numbers - The array of numbers
+ * @returns {number} The average of the numbers
+ */
+function calculateAverage   (arr){
+
+     if (arr.length === 0) return 0; // Tránh chia cho 0
+    let avgOfArrayNumber= sumArray(arr)/(arr.length);
+    return avgOfArrayNumber;
+}
+
+// Sample usage
+ console.log(calculateAverage([10, 20, 30, 40, 50])); // 30
