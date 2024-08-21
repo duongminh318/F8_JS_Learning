f044a0550c80c7ee8c4caed0335fafe9d9ab6e87
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-08-21 16:20:18 +0700

    completed ex3 loop number

--- 4. f8_js_pro/loop.js
+++ 4. f8_js_pro/loop.js
@@ -135,3 +135,76 @@ function calculateAverage   (arr){
 
 // Sample usage
  console.log(calculateAverage([10, 20, 30, 40, 50])); // 30
+
+/**
+ * Tổng hợp bài tập vòng lặp For #3 
+1. Viết hàm isAscending nhận vào một mảng số và kiểm tra xem mảng đó có phải là dãy số tăng dần không. Trường hợp mảng chỉ có một phần tử hoặc
+ mảng rỗng không được coi là mảng tăng dần.
+2. Viết hàm isPrime nhận vào một số nguyên n và trả về true 
+nếu n là số nguyên tố, ngược lại trả về false.
+3. Viết hàm sumPrimes nhận vào hai số nguyên start và end, 
+sau đó tính và trả về tổng của tất cả các số nguyên tố trong khoảng từ start đến end.
+ */
+
+/**
+ * 1. Checks if an array is strictly ascending.
+ *
+ * @param {Array<number>} arr - The array to check
+ * @returns {boolean} True if the array is ascending, false otherwise
+ */
+const isAscending = (arr) => {
+    if (arr.length === 0 || arr.length === 1) {
+        return false;
+    } else {
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] >= arr[i + 1]) {
+                return false; // Nếu bất kỳ phần tử nào lớn hơn phần tử kế tiếp, mảng không tăng dần
+            }
+        }
+        return true; // Nếu không có phần tử nào vi phạm, mảng là tăng dần
+    }
+};
+
+// Sample usage
+console.log(isAscending([1, 2, 3, 5])); // true
+
+/**
+ * 2. Checks if a number is prime.
+ *
+ * @param {number} n - The number to check
+ * @returns {boolean} True if n is a prime number, false
+ * - chia hết cho 1 và chính nó
+ * otherwise
+ */
+const isPrime = (numberCheck) => {
+    if (numberCheck <= 1) return false; // Các số nhỏ hơn hoặc bằng 1 không phải là số nguyên tố
+    for (let i = 2; i <= Math.sqrt(numberCheck); i++) {
+        if (numberCheck % i === 0) {
+            return false; // Nếu tìm thấy một ước số, không phải là số nguyên tố
+        }
+    }
+    return true; // Nếu không tìm thấy ước số nào, đây là số nguyên tố
+};
+
+
+// Sample usage
+// console.log(isPrime(2)); // true
+
+/**
+ * 3. Sums all prime numbers in a given range.
+ *
+ * @param {number} start - The start of the range
+ * @param {number} end - The end of the range
+ * @returns {number} The sum of prime numbers in the range
+ */
+const sumPrimes  = (start , end) => {
+  let sumPrimes =0;
+    for (let i = start; i < end; i++) {
+        if (isPrime(i)) {
+          sumPrimes  +=i;
+        }
+    }
+   return sumPrimes 
+};
+// Sample usage
+// console.log(sumPrimes(10, 20)); // 60
