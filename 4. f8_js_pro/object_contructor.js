452e6e00c4bc8079e1d0e613f4c366b3167c6061
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-08-27 11:17:50 +0700

    add counterRate, phần trăm phản công

--- 4. f8_js_pro/object_contructor.js
+++ 4. f8_js_pro/object_contructor.js
@@ -21,6 +21,13 @@ function Character(name, hp, atk, defense, speed, counterRate) {
         const damage = Math.max(this.atk - target.defense, 0);
         target.hp -= damage;
         console.log(`${this.name} tấn công ${target.name}, gây ra ${damage} sát thương. ${target.name} còn lại ${target.hp} máu.`);
+
+          // xử lý target phản công
+          if(target.isAlive() && Math.random()<target.counterRate){
+            let counterDamage= Math.max(target.atk - this.defense, 0);// damamge phản công
+            this.hp-=counterDamage;
+            console.log(`${target.name} "phang lại" ${this.name}, gây ra ${counterDamage} sát thương. ${this.name} còn lại ${this.hp} máu.`);
+        }
     };
     this.isAlive = function() {
         return this.hp > 0;
@@ -31,24 +38,28 @@ function Character(name, hp, atk, defense, speed, counterRate) {
 // Bắt đầu vòng lặp chiến đấu
 function battle(char1, char2) {
     let round = 1;
-
+    
     while (char1.isAlive() && char2.isAlive()) {
         console.log(`Round ${round}:`);
-
-        if (char1.speed > char2.speed) {
-            char1.attack(char2);
-            if (char2.isAlive()) char2.attack(char1);
+    
+        if (char1.speed >= char2.speed) {
+            battleRound(char1, char2);
         } else {
-            char2.attack(char1);
-            if (char1.isAlive()) char1.attack(char2);
+            battleRound(char2, char1);
         }
-
+    
         round++;
     }
-    // kiểm tra xem ai là người chiến thắng
+    
     const winner = char1.isAlive() ? char1 : char2;
     console.log(`${winner.name} wins!`);
     return winner;
+}
+
+// xử lý luột đánh
+function battleRound(attacker, defender) {
+    attacker.attack(defender);
+    if (defender.isAlive() && attacker.isAlive()) defender.attack(attacker);
 }
 
 // Tạo 2 nhân vật để tham gia trận chiến
